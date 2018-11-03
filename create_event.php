84e2d7c2ee98f5fee5b2dd520d4a2d84165dcde1
rtomyj <z1813273@hopper.cs.niu.edu>
2018-11-03 17:32:32 -0500

    changed layout, added bootstrap elelments

--- create_event.php
+++ create_event.php
@@ -13,10 +13,10 @@ printFooter();
 
 function createAgent(){
 ?>
-	<div style='margin-left: 20px; margin-right: 20px; margin-top: 20px'>
+	<div style='margin-left: 15px; margin-right: 15px; margin-top: 15px'>
 		<div>
 			<h4 style='display: inline; font-size: 45px'>Create New Event</h4>
-			<h6 style='display: inline; margin-left: 8px; font-size: 25px;'>Event ID</h6>
+			<h6 style='display: inline; margin-left: 8px; font-size: 30px;'>Event ID</h6>
 		</div>
 		<br><br>
 		<form class='form'>
@@ -25,148 +25,153 @@ function createAgent(){
 				<div class='card-body container-fluid'>
 					<h4 class='card-title'>Performer</h4>
 					<div class='row' style='margin-top: 16px; margin-bottom: 16px'>
-						<div class='row col-lg-4'>
-							<div class='col-sm-6'>
-							</div>
-							<div class='col-sm-3'>
-								<input type='radio' name='performer' value='Artist' > Artist
-							</div>
-							<div class='col-sm-3'>
-								<input type='radio' name='performer' value='Band' > Band
-							</div>
+
+						
+						<div class='col-md-2 offset-md-2'>
+							<label class="form-check-label">
+								<input class="form-check-input" type="radio" name="performer" checked='true'>
+								Artist
+							</label>
+						</div>
+						<div class='col-md-2'>
+							<label class="form-check-label">
+								<input class="form-check-input" type="radio" name="performer" value="">
+								Band
+							</label>
 						</div>
 					</div>
+
+				
 					<div class='row' style='margin-top: 16px; margin-bottom: 16px'>
-						<div class='row col-lg-4'>
-							<div class='col-sm-6'>
-								<p class="label">Name:</p>
-							</div>
-							<div class='col-sm-6'>
-								<select name='state' class='label'>
-									<option value='IL'>Artist/Band Name</option>
-								</select>
-							</div>
+
+						<div class='col-md-2'>
+							<p class="label">Name:</p>
 						</div>
+						<div class='col-md-6'>
+							<select name='state' class='custom-select form-control-sm'>
+								<option value='IL'>Artist/Band Name</option>
+							</select>
+						</div>
+
 					</div>
 				</div>
 			</div>
 
-			<div class='card' style='margin-top: 25px;'>
+			<div class='card' style='margin-top: 30px;'>
 				<div class='card-body container-fluid'>
 					<h4 class='card-title'>Event Details</h4>
-					<div class='row' style='margin-top: 16px; margin-bottom: 16px;'>
-						<div class='row col-lg-4'>
-							<div class='col-sm-6'>
-								<p class='label'>Date Created:</p>
-							</div>
-							<div class='col-sm-6'>
-							<input input='text' name='datecreated' value="<?php echo date('m/d/Y');?>"  readonly>
-							</div>
+
+					<div class='row' style='margin-top: 16px; margin-bottom: 16px'>
+
+						<div class='col-md-2'>
+							<p class='label'>Date Created:</p>
 						</div>
-						<div class='row col-lg-4'>
-							<div class='col-sm-6'>
-								<p class='label'>Event Date:</p>
-							</div>
-							<div class='col-sm-6'>
-								<input type='date' id="datepicker" name='eventdate' size='9' value="" />
-							</div>
+						<div class='col-md-4'>
+							<input input='text' name='datecreated' value="<?php echo date('m/d/Y', round(microtime(true)));?>"  readonly>
 						</div>
-						<div class='row col-lg-4'>
-							<div class='col-sm-6'>
-								<p class='label'>Start Time:</p>
-							</div>
-							<div class='col-sm-6'>
-								<input type='time' name='starttime' >
-							</div>
+
+					</div>
+
+
+					<div class='row' style='margin-top: 16px; margin-bottom: 16px'>
+						<div class='col-md-2'>
+							<p class='label'>Event Date:</p>
+						</div>
+						<div class='col-md-4'>
+							<input type='date' id="datepicker" name='eventdate' value="<?php echo date('m/d/Y', round(microtime(true) + 604800));?>" />
 						</div>
 					</div>
 
 
+					<div class='row' style='margin-top: 16px; margin-bottom: 16px'>
+						<div class='col-md-2'>
+							<p class='label'>Start Time:</p>
+						</div>
+						<div class='col-md-4'>
+							<input type='time' name='starttime' value='<?php echo date("h:i:sa");?>' >
+						</div>
+					</div>
+
+
+
 					<div class='row' style='margin-top: 16px; margin-bottom: 16px;'>
-						<div class='col-lg-4 row'>
-							<div class='col-sm-6'>
-								<p class='label'>Location:</p>
-							</div>
-							<div class='col-sm-6'>
-								<select name='location' class='label'>
-									<option value='event'>The Event Center, 483 Block St. Aurora, IL</option>
-								</select>
-							</div>
+
+						<div class='col-md-2'>
+							<p class='label'>Location:</p>
+						</div>
+						<div class='col-md-6'>
+							<select name='location' class='custom-select form-control-sm'>
+								<option value='event'>The Event Center, 483 Block St. Aurora, IL</option>
+							</select>
 						</div>
 
-						<div class='col-lg-4 row'>
-							<div class='col-sm-6'>
-								<p class='label'>Capacity:</p>
-							</div>
-							<div class='col-sm-6'>
-								<input input='text' name='capacity' class='label'>
-							</div>
+
+						<div class='col-md-2'>
+							<p class='label'>Capacity:</p>
 						</div>
+						<div class='col-md-2'>
+							<input input='text' name='capacity' class='label'>
+						</div>
+
 
 					</div>
 	
 					<div class='row' style='margin-top: 16px; margin-bottom: 16px;'>
-						<div class='col-lg-4 row'>
-							<div class='col-sm-6'>
+
+							<div class='col-md-2'>
 								<p class='label'>Vendors:</p>
 							</div>
-							<div class='col-sm-6'>
-								<select name='agenttype' class='label'>
+							<div class='col-md-6'>
+								<select name='agenttype' class='custom-select form-control-sm'>
 									<option value='for_artist'>For Artist</option>
 									<option value='for_band'>For Band</option>
 									<option value='other'>Other</option>
 								</select>
 							</div>
-						</div>
+
 					</div>
 
 					<div class='row' style='margin-top: 16px; margin-bottom: 16px;'>
-						<div class='col-lg-4 row'>
-							<div class='col-sm-6'>
+
+							<div class='col-md-2'>
 								<p class='label'>Manager:</p>
 							</div>
-							<div class='col-sm-6'>
-								<select name='agenttype' class='label'>
+							<div class='col-md-6'>
+								<select name='agenttype' class='custom-select form-control-sm'>
 									<option value='for_artist'>For Artist</option>
 									<option value='for_band'>For Band</option>
 									<option value='other'>Other</option>
 								</select>
 							</div>
-						</div>
+
 					</div>
 				</div>
 			</div>
 
 
-			<div class='card' style='margin-top: 25px;'>
+			<div class='card' style='margin-top: 30px;'>
 				<div class='card-body container-fluid'>
 					<h4 class='card-title'>Special Notes</h4>
 					<div class='row' style='margin-top: 16px; margin-bottom: 16px'>
-						<div class='col-lg-4 row'>
-							<div class='col-sm-6'>
-							</div>
-							<div class='col-sm-6'>
-								<textarea name='comment' cols="100" align="center"> </textarea>
-							</div>
-						</div>
+
+						<textarea name='comment' class='col-md-10 offset-md-2' rows='5'> </textarea>
+
 					</div>
 				</div>
 
 			</div>
-			<div class='row container-fluid' style='margin-top: 45px;'>
-				<div class='col-sm-1'>
-					<button class='btn btn-outline-danger'>
-						Cancel
-					</button>
+
+
+			<div class='row container-fluid' style='margin-top: 30px;'>
+				<div class='col-md-2'>
+					<input class='btn btn-outline-danger' id="btntest" type="button" value="Cancel" onclick="window.location.href='index.php'">
 				</div>
-				<div class=col-sm-10>
-				</div>
-				<div class='col-sm-1'>
-					<button class='btn btn-success'>
-						Create
-					</button>
+				<div class='col-md-2 offset-md-8'>
+					<input type='submit' class='btn btn-success' value='Create Agent'>
 				</div>
 			</div>
+
+
 		</form>
 
 
