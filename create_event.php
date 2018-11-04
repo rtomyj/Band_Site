3378643e26b419587288fcd7c2b5ce92c96ab19b
rtomyj <z1813273@hopper.cs.niu.edu>
2018-11-04 08:58:48 -0600

    finished event page

--- create_event.php
+++ create_event.php
@@ -3,6 +3,26 @@ require_once('common_php/header_footer.php');
 require_once('../includes/conn.php');
 
 printHeader("GEM - Create Event");
+
+
+if (!empty($_POST)){
+	if ($_POST['performer'] == 'artist'){
+		$sql = "INSERT INTO event(artist_id, performer_type, date_created, event_date, start_time, event_location, capacity, event_manager, stage_vendor, equipment_vendor, lighting_vendor, sound_vendor, notes, event_status) VALUES({$_POST['artist']}, 'A', '{$_POST['dateCreated']}', '{$_POST['eventDate']}', '{$_POST['startTime']}', {$_POST['location']}, {$_POST['capacity']}, {$_POST['manager']}, {$_POST['stageVendor']}, {$_POST['equipmentVendor']}, {$_POST['lightingVendor']}, {$_POST['soundVendor']}, '{$_POST['notes']}', 'Created'  )";
+	}
+	else{
+		$sql = "INSERT INTO event(band_id, performer_type, date_created, event_date, start_time, event_location, capacity, event_manager, stage_vendor, equipment_vendor, lighting_vendor, sound_vendor, notes, event_status) VALUES({$_POST['band']}, 'B', '{$_POST['dateCreated']}', '{$_POST['eventDate']}', '{$_POST['startTime']}', {$_POST['location']}, {$_POST['capacity']}, {$_POST['manager']}, {$_POST['stageVendor']}, {$_POST['equipmentVendor']}, {$_POST['lightingVendor']}, {$_POST['soundVendor']}, '{$_POST['notes']}', 'Created'  )";
+	}
+	
+
+	echo $sql;
+	$conn->exec($sql);
+	
+	header ('Location: ' . $_SERVER['REQUEST_URI']);
+	exit();
+
+}
+
+
 createAgent($conn);
 
 ?>
@@ -22,6 +42,7 @@ function createAgent($conn){
 			artist.style.display = (artist.style.display == 'none') ? 'block' : 'none'
 		}
 
+
 	</script>
 	<div style='margin-left: 15px; margin-right: 15px; margin-top: 15px'>
 		<div>
@@ -29,7 +50,7 @@ function createAgent($conn){
 			<h6 style='display: inline; margin-left: 8px; font-size: 30px;'>Event ID</h6>
 		</div>
 		<br><br>
-		<form class='form'>
+		<form class='form' name='eventForm' action="create_event.php" method="post">
 
 			<div class='card'>
 				<div class='card-body container-fluid'>
@@ -38,14 +59,14 @@ function createAgent($conn){
 
 						
 						<div class='col-md-2 offset-md-2'>
-							<label class="form-check-label">
-								<input class="form-check-input" type="radio" name="performer" onclick="switchArtistBand();" checked='true'>
+							<label class="form-check-label" id='artistLabel'>
+								<input class="form-check-input" type="radio" name="performer" value='artist' onclick="switchArtistBand();" checked>
 								Artist
 							</label>
 						</div>
 						<div class='col-md-2'>
-							<label class="form-check-label">
-								<input class="form-check-input" type="radio" name="performer" value="" onclick="switchArtistBand();">
+							<label class="form-check-label" id='bandLabel'>
+								<input class="form-check-input" type="radio" name="performer" value="band" onclick="switchArtistBand();">
 								Band
 							</label>
 						</div>
@@ -58,11 +79,10 @@ function createAgent($conn){
 							<p class="label">Name:</p>
 						</div>
 						<div class='col-md-6' id='artistDiv'>
-							<select name='state' class='custom-select form-control-sm'>
+							<select name='artist' class='custom-select form-control-sm'>
 								<?php
 								$sql = 'SELECT artist_id, first_name, middle_initial, last_name from artist ORDER BY first_name';
 								$query = $conn -> query($sql);
-								echo $sql;
 								while ($result = $query -> fetch()){
 									if ($result['middle_initial'] != '')
 										$name = "{$result['first_name']} {$result['middle_initial']}. {$result['last_name']}";
@@ -76,11 +96,10 @@ function createAgent($conn){
 						</div>
 
 						<div class='col-md-6' style='display:none;' id='bandDiv'>
-							<select name='state' class='custom-select form-control-sm'>
+							<select name='band' class='custom-select form-control-sm'>
 								<?php
 								$sql = 'SELECT band_id, band_name from band ORDER BY band_name';
 								$query = $conn -> query($sql);
-								echo $sql;
 
 								while ($result = $query -> fetch()){
 									$name = "{$result['band_name']}";
@@ -105,7 +124,7 @@ function createAgent($conn){
 							<p class='label'>Date Created:</p>
 						</div>
 						<div class='col-md-4'>
-							<input input='text' name='datecreated' value="<?php echo date('m/d/Y', round(microtime(true)));?>"  readonly>
+							<input type='date' name='dateCreated' value="<?php echo date('Y-m-d', round(microtime(true)));?>"  readonly>
 						</div>
 
 					</div>
@@ -116,7 +135,7 @@ function createAgent($conn){
 							<p class='label'>Event Date:</p>
 						</div>
 						<div class='col-md-4'>
-							<input type='date' id="datepicker" name='eventdate' value="<?php echo date('m/d/Y', round(microtime(true) + 604800));?>" />
+							<input type='date' id="datepicker" name='eventDate' value="<?php echo date('Y-m-d', round(microtime(true) + 604800));?>" />
 						</div>
 					</div>
 
@@ -126,7 +145,7 @@ function createAgent($conn){
 							<p class='label'>Start Time:</p>
 						</div>
 						<div class='col-md-4'>
-							<input type='time' name='starttime' value='<?php echo date("h:i:sa");?>' >
+							<input type='time' name='startTime' value='<?php echo date("h:i");?>' >
 						</div>
 					</div>
 
@@ -157,7 +176,7 @@ function createAgent($conn){
 							<p class='label'>Capacity:</p>
 						</div>
 						<div class='col-md-2'>
-							<input input='text' name='capacity' class='label'>
+							<input type='text' name='capacity' class='label' required>
 						</div>
 
 
@@ -169,7 +188,7 @@ function createAgent($conn){
 							<p class='label'>Manager:</p>
 						</div>
 						<div class='col-md-6'>
-							<select name='agenttype' class='custom-select form-control-sm'>
+							<select name='manager' class='custom-select form-control-sm'>
 								<?php
 								$sql = "SELECT manager_id, first_name, middle_initial, last_name FROM manager ORDER BY first_name";
 								$query = $conn -> query($sql);
@@ -201,7 +220,7 @@ function createAgent($conn){
 							<p class='label'>Stage Setup:</p>
 						</div>
 						<div class='col-md-6'>
-							<select name='agenttype' class='custom-select form-control-sm'>
+							<select name='stageVendor' class='custom-select form-control-sm'>
 								<?php
 									$sql = "SELECT vendor_id, vendor_name FROM vendor where vendor_type = 'Stage Setup' ORDER BY vendor_name";
 									$query = $conn -> query($sql);
@@ -223,7 +242,7 @@ function createAgent($conn){
 							<p class='label'>Equipment:</p>
 						</div>
 						<div class='col-md-6'>
-							<select name='agenttype' class='custom-select form-control-sm'>
+							<select name='equipmentVendor' class='custom-select form-control-sm'>
 								<?php
 									$sql = "SELECT vendor_id, vendor_name FROM vendor where vendor_type = 'Equipment' ORDER BY vendor_name";
 									$query = $conn -> query($sql);
@@ -245,7 +264,7 @@ function createAgent($conn){
 							<p class='label'>Lighting:</p>
 						</div>
 						<div class='col-md-6'>
-							<select name='agenttype' class='custom-select form-control-sm'>
+							<select name='lightingVendor' class='custom-select form-control-sm'>
 								<?php
 									$sql = "SELECT vendor_id, vendor_name FROM vendor where vendor_type = 'Lighting' ORDER BY vendor_name";
 									$query = $conn -> query($sql);
@@ -268,7 +287,7 @@ function createAgent($conn){
 							<p class='label'>Sound:</p>
 						</div>
 						<div class='col-md-6'>
-							<select name='agenttype' class='custom-select form-control-sm'>
+							<select name='soundVendor' class='custom-select form-control-sm'>
 							<?php
 								$sql = "SELECT vendor_id, vendor_name FROM vendor where vendor_type = 'Sound' ORDER BY vendor_name";
 								$query = $conn -> query($sql);
@@ -292,7 +311,7 @@ function createAgent($conn){
 					<h4 class='card-title'>Special Notes</h4>
 					<div class='row' style='margin-top: 16px; margin-bottom: 16px'>
 
-						<textarea name='comment' class='col-md-10 offset-md-2' rows='5'> </textarea>
+						<textarea name='notes' class='col-md-10 offset-md-2' rows='5'> </textarea>
 
 					</div>
 				</div>
@@ -305,11 +324,11 @@ function createAgent($conn){
 					<input class='btn btn-outline-danger' id="btntest" type="button" value="Cancel" onclick="window.location.href='index.php'">
 				</div>
 				<div class='col-md-2 offset-md-8'>
-					<input type='submit' class='btn btn-success' value='Create Agent'>
+					<input type='submit' class='btn btn-success'>
 				</div>
 			</div>
 
-
+		<input type="hidden" name="submitted" value="true" />
 		</form>
 
 
